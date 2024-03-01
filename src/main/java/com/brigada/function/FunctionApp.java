a732af6e38a2fe6b66b4656aab28a3b634e64a68
Dead <space.gee.z@gmail.com>
2024-03-01 15:13:21 +0300

    add sinPowerSeries

--- src/main/java/com/brigada/function/FunctionApp.java
+++ src/main/java/com/brigada/function/FunctionApp.java
@@ -2,8 +2,27 @@ package com.brigada.function;
 
 public class FunctionApp {
 
-    public int add(int a, int b) {
-        return a + b;
+    private double eps = 0.5;
+    private double x = 0.147;
+    private double sum = 0;
+    private int n = 0;
+    private double current = Math.abs(Math.pow(x, (2*n-1))/getFactorial(n));
+
+    private int getFactorial(int n){
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
     }
 
+    public double getSinPowerSeries(){
+        while(current < eps){
+            x = x * (- Math.pow(x, 2)/(2*n*(n+1)));
+            sum += x;
+            n++;   
+            current = Math.abs(Math.pow(x, (2*n-1))/getFactorial(n));
+        }
+        return sum;
+    }  
 }
