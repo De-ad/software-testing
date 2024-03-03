9760a292c9c358bcf68ee06d53f2352b31ae46ba
Dead <space.gee.z@gmail.com>
2024-03-03 03:05:19 +0300

    Add tests

--- src/main/java/com/brigada/function/FunctionApp.java
+++ src/main/java/com/brigada/function/FunctionApp.java
@@ -2,13 +2,9 @@ package com.brigada.function;
 
 public class FunctionApp {
 
-    private double eps = 0.5;
-    private double x = 0.147;
-    private double sum = 0;
-    private int n = 0;
-    private double current = Math.abs(Math.pow(x, (2*n-1))/getFactorial(n));
+   
 
-    private int getFactorial(int n){
+    int getFactorial(int n){
         int result = 1;
         for (int i = 1; i <= n; i++) {
             result = result * i;
@@ -16,13 +12,19 @@ public class FunctionApp {
         return result;
     }
 
-    public double getSinPowerSeries(){
-        while(current < eps){
-            x = x * (- Math.pow(x, 2)/(2*n*(n+1)));
-            sum += x;
-            n++;   
-            current = Math.abs(Math.pow(x, (2*n-1))/getFactorial(n));
-        }
+    public double getSinPowerSeries(double x, double eps){
+        double sum = 0;
+        int n = 0;
+        double current;
+        double temp;
+        do {
+            temp = Math.pow(-1, n) * Math.pow(x, 2 * n + 1) / getFactorial(2 * n + 1);
+            sum += temp;
+            n++;
+            current = Math.abs(temp);
+
+        } while (current >= eps);
+
         return sum;
     }  
 }
