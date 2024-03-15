4986c20a76fc2b4855a714f3d9b45154a4f88fd1
IndianMax03 <vermamanu@mail.ru>
2024-03-16 01:00:50 +0300

    Init: lab#2

---
+++ src/main/java/com/brigada/Main.java
@@ -0,0 +1,7 @@
+package com.brigada;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("LEST GO LAB#2");
+    }
+}
---
+++ src/test/java/com/brigada/MainTest.java
@@ -0,0 +1,14 @@
+package com.brigada;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+
+}
