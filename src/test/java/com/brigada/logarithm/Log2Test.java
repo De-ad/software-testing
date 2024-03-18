7319801845208995b733c577d861c305c1c69f97
IndianMax03 <vermamanu@mail.ru>
2024-03-18 17:46:57 +0300

    Add: stub example

--- src/test/java/com/brigada/logarithm/Log2Test.java
+++ src/test/java/com/brigada/logarithm/Log2Test.java
@@ -10,21 +10,23 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 public class Log2Test {
 
-    @Mock
-    private static Ln mockedLn;
-    private static Map<String, Double> lnValues = new HashMap<>();
-    private static Ln ln;
-    private static LogN log2WithMock;
-    private static LogN log2;
     private static final int BASE = 2;
     private final double SCALE = Math.pow(10, 11);
+    @Mock
+    private static Ln mockedLn = new Ln();
+    private static LogN log2WithMock = new LogN(mockedLn, BASE);
+    private static Map<String, Double> lnValues = new HashMap<>();
+    private static Ln ln;
+    private static LogN log2;
 
     @BeforeAll
     public static void init() {
+
         ln = new Ln();
         log2 = new LogN(ln, BASE);
         log2WithMock = new LogN(mockedLn, BASE);
@@ -32,18 +34,16 @@ public class Log2Test {
         scanner.nextLine();
         while (scanner.hasNext()) {
             String cur = scanner.nextLine();
-            String x = cur.split(",")[0];
+            double x = Double.parseDouble(cur.split(",")[0]);
             double y = Double.parseDouble(cur.split(",")[1]);
-            lnValues.put(x, y);
+            when(mockedLn.calculate(x)).thenReturn(y);
         }
-        when(mockedLn.calculate(Mockito.anyDouble())).thenAnswer(invocationOnMock -> lnValues.get(String.valueOf(invocationOnMock.getArgument(0))));
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = {"/tables/Log2Table.csv"}, numLinesToSkip = 1, delimiter = ',', useHeadersInDisplayName = true)
     public void equivalenceAnalysis(double x, double y) {
-        System.out.println(y + " | " + log2WithMock.calculate(x));
-//        assertEquals(log2WithMock.calculate(x), y);
+        assertEquals(Math.round(log2WithMock.calculate(x) * SCALE) / SCALE, Math.round(y * SCALE) / SCALE);
     }
 
 }
